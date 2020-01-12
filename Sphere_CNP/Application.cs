30910bd3312fea3ca125fc80ec670b138548e27f
Christina Porter <45431770+cporterbellarmine@users.noreply.github.com>
2020-01-12 14:59:26 -0500

    First commit.

---
+++ Sphere_CNP/Application.cs
@@ -0,0 +1,50 @@
+﻿/**
+ * This is the application of the class Sphere.
+ * @author cporter
+ * @version 1/12/2020
+ * CS-311 Programming Project 1
+ * Spring 2020
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sphere_CNP
+{
+    class Application
+    {
+        static void Main(string[] args)
+        {
+
+            Sphere s1 = new Sphere();
+            Console.WriteLine("Testing my empty constructor:");
+            Console.WriteLine();
+            Console.WriteLine($"My empty constructor has a diameter value of {s1.GetDiameter()}, a" +
+                $" surface area of {Math.Round(s1.GetSurfaceArea(), 2)}, and a volume of {Math.Round(s1.GetVolume(), 2)}.");
+
+            Sphere s2 = new Sphere(1);
+            Sphere s3 = new Sphere(5);
+            Sphere s4 = new Sphere(20);
+
+            Console.WriteLine();
+            Console.WriteLine("Testing my bounds setters for radius:");
+            Console.WriteLine();
+            Console.WriteLine($"My entered diameter value is 1. The value after bounds checking" +
+                $" is {s2.GetDiameter()}, the surface area is {Math.Round(s2.GetSurfaceArea(), 2)}," +
+                $" and the volume is {Math.Round(s2.GetVolume(), 2)}.");
+
+            Console.WriteLine();
+            Console.WriteLine("Other values:");
+            Console.WriteLine();
+            Console.WriteLine($"My sphere has a diameter value of {s3.GetDiameter()}," +
+                $" a surface area of {Math.Round(s3.GetSurfaceArea(), 2)}, and a volume of {Math.Round(s3.GetVolume(), 2)}.");
+            Console.WriteLine($"My sphere has a diameter value of {s4.GetDiameter()}," +
+                $" a surface area of {Math.Round(s4.GetSurfaceArea(), 2)}, and a volume of {Math.Round(s4.GetVolume(), 2)}.");
+            Console.WriteLine();
+
+        }//end main
+    }//end class
+}//end namespace
---
+++ Sphere_CNP/Sphere.cs
@@ -0,0 +1,88 @@
+﻿/**
+ * This class models the Sphere class from the Java language to C#.
+ * @author Rob Kelley
+ * @author cporter
+ * @version 1/12/2020
+ * CS-311 Programming Project 1
+ * Spring 2020
+ */
+
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sphere_CNP
+{
+    class Sphere
+    {
+        private double diameter;
+
+        /**
+	     * Empty-argument constructor to put object
+	     * into a consistent state.
+	     */
+        public Sphere()
+        {
+            diameter = 2.0;
+        }//end constructor
+
+        /**
+	     * Constructor accepts value of diameter.
+	     * Preferred constructor.
+	     */
+        public Sphere(double diameter)
+        {
+            this.SetDiameter(diameter);
+        }//end constructor
+
+        /**
+	     * Calculates the volume of the sphere.
+	     * @return volume
+	     */
+        public double GetVolume()
+        {
+            return (4.0 / 3.0) * Math.PI * Math.Pow(diameter / 2, 3);
+        }//end GetVolume
+
+        /**
+	     * Calculates the surface area of the sphere.
+	     */
+        public double GetSurfaceArea()
+        {
+            return 4.0 * Math.PI * Math.Pow(diameter / 2, 2);
+        }//end GetSurfaceArea
+
+        /**
+         * Getter for diameter
+         */
+        public double GetDiameter()
+        {
+            return diameter;
+        }//end GetDiameter
+
+        /**
+         * Setter for diameter. Uses bounds checking
+         * to ensure that the value is no less than 2.
+         */
+        public void SetDiameter(double diameter)
+        {
+            if(diameter < 2)
+            {
+                this.diameter = 2;
+            }//end if
+            else
+            {
+                this.diameter = diameter;
+            }//end else
+        }//end SetDiameter
+
+        override
+        public String ToString()
+        {
+            return "Sphere [diameter = " + diameter + "]";
+        }//end ToString
+    }//end class
+}//end namespace
